d02e71c591c615c3e601a1876c626bd832268a03
iyanzzzz <alviannr0611@gmail.com>
2024-10-15 19:49:16 +0700

    tugas week 4

---
+++ WEEEK4 WEB/script.js
@@ -0,0 +1,34 @@
+let displayValue = '';
+
+function appendNumber(number) {
+    displayValue += number;
+    updateDisplay();
+}
+
+function appendOperator(operator) {
+    displayValue += operator;
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = '';
+    updateDisplay();
+}
+
+function deleteLast() {
+    displayValue = displayValue.slice(0, -1);
+    updateDisplay();
+}
+
+function calculateResult() {
+    try {
+        displayValue = eval(displayValue).toString();
+    } catch {
+        displayValue = 'Error';
+    }
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById('display').value = displayValue;
+}
