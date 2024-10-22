9db45d209409c37e47f10daaf868617c6552720f
Imjustzero <imjustzero@duck.com>
2024-10-22 10:23:21 +0700

    commit

--- web/week6/src/script.js
+++ web/week6/src/script.js
@@ -17,5 +17,17 @@ const janji = new Promise((resolve, reject) => {
 });
 
 console.log("mulai");
+// console.log(janji);
+// kalau janji masuk ke respon, kalau ingkar ke catch
 console.log(janji);
+janji
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((respons) => {
+    console.error("salah");
+  })
+  .finally((responsn) => {
+    console.log("finally");
+  });
 console.log("selesai");
