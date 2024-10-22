15c3286d28d16255451c9df4e145c8c05934b779
Imjustzero <imjustzero@duck.com>
2024-10-22 10:34:59 +0700

    commit

--- web/week6/src/script.js
+++ web/week6/src/script.js
@@ -1,7 +1,8 @@
 // rejected resolve
 let stats = true;
+let time = 1000;
 
-const janji = new Promise((resolve, reject) => {
+const janji1 = new Promise((resolve, reject) => {
   if (stats) {
     setTimeout(() => {});
     resolve({
@@ -16,18 +17,43 @@ const janji = new Promise((resolve, reject) => {
   }
 });
 
-console.log("mulai");
+const janji2 = new Promise((resolve, reject) => {
+  if (stats) {
+    setTimeout(() => {});
+    resolve(
+      {
+        message: "Ok!",
+        code: 200,
+      },
+      time
+    );
+  } else {
+    reject(
+      {
+        message: "bad request",
+        code: 400,
+      },
+      time
+    );
+  }
+});
+
+Promise.all([janji1, janji2]).then((values) => {
+  console.log(values);
+});
+
+// console.log("mulai");
+// // console.log(janji);
+// // kalau janji masuk ke respon, kalau ingkar ke catch
 // console.log(janji);
-// kalau janji masuk ke respon, kalau ingkar ke catch
-console.log(janji);
-janji
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((respons) => {
-    console.error("salah");
-  })
-  .finally((responsn) => {
-    console.log("finally");
-  });
-console.log("selesai");
+// janji
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((respons) => {
+//     console.error("salah");
+//   })
+//   .finally((responsn) => {
+//     console.log("finally");
+//   });
+// console.log("selesai");
