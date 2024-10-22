2ff04c0ef1b460ee840dfb8516dc8feec0e4299e
unknown <sigaralam.pandu@gmail.com>
2024-10-22 22:31:02 +0700

    week5

---
+++ week 5/promise.js
@@ -0,0 +1,58 @@
+// reject resolve
+let.time = values;
+let stats = true;
+const janji1 = new Promise((resolve, reject) => {
+  if (stats) {
+    setTimeout(() => {
+      resolve({
+        massage: "OK!",
+        code: 200,
+      });
+    });
+  } else {
+    setTimeout(() => {
+      reject({
+        massage: "bad request",
+        code: "200",
+      });
+    });
+  }
+});
+
+const janji2 = new Promise((resolve, reject) => {
+    
+  if (stats) {
+    setTimeout(() => {
+      resolve({
+        massage: "OK!",
+        code: 200,
+      });
+    });
+  } else {
+    setTimeout(() => {
+      reject({
+        massage: "bad request",
+        code: "200",
+      });
+    },time);
+  }
+});
+
+
+
+Promise.all([janji1,janji2]).then((values) =>{
+    console.log(values);
+})
+
+
+//console.log("mulai");
+//janji1.then(
+  //  respone =>{
+    //    console.log(respone)
+    //}
+//).catch(
+  //  respone =>{
+    //    console.error("salah")
+   // }
+//);
+//console.log("selesai");
