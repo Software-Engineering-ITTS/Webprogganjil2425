fb0a6fa1126d8342d1209144ac556f195c805eb2
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-10-22 18:19:42 +0700

    promise 2

--- week5/promise.js
+++ week5/promise.js
@@ -3,6 +3,8 @@
 // fetch() => Promise
 
 let stats = true;
+let time = 5000;
+
 const janji1 = new Promise(
     (resolve, reject) => {
         if (stats) {
@@ -29,11 +31,46 @@ const janji1 = new Promise(
     }
 );
 
-console.log("mulai")
-console.log(janji1)
-janji1.then(
-    response => { console.log(response) }
-).catch(
-    failure => { console.error(failure) }
+const janji2 = new Promise(
+    (resolve, reject) => {
+        if (stats) {
+            setTimeout(
+                () => {
+                    resolve({
+                        message: "Ok!",
+                        code: 200,
+                    })
+                }, time
+            )
+
+        } else {
+            setTimeout(
+                () => {
+                    reject({
+                        message: "bad request",
+                        code: 400,
+                    })
+                }, time
+            )
+
+        }
+    }
 );
-console.log("selesai")
+
+Promise.all([janji1, janji2]).then(
+    (values) => {
+        console.log(values);
+    }
+);
+// console.log("mulai")
+// console.log(janji1)
+// janji1.then(
+//     response => { console.log(response) }
+// ).catch(
+//     failure => { console.error(failure) }
+// ).finally(
+//     response => {
+//         console.log("finally")
+//     }
+// );
+// console.log("selesai")
