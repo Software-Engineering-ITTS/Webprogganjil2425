afbb7b2393c0351240a7b1f2d7708ca6e34c38cd
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-11-05 19:05:05 +0700

    done

--- otsc/script.js
+++ otsc/script.js
@@ -61,4 +61,36 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     })
 
+    document.getElementById("btnSubmit").addEventListener('click', function (event) {
+        const noVerif = document.getElementById("inpVerifikasiPegawai")
+        if(noVerif.value === ''){
+            alert("Masukkan No Verifikasi dulu")
+            return
+        }
+        // const text = "Apakah Anda Yakin Ingin Mengosongkan Form?"
+        // if (confirm(text) === true) {
+
+        //     document.getElementById("txtKodeBarang").value = ""
+        //     document.getElementById("txtNamaBarang").value = ""
+
+        //     document.getElementById("selectCategory").selectedIndex = 0
+        //     document.getElementById("tanggalExpiredForm").hidden = true
+
+        //     document.getElementById("inpNewStock").hidden = true
+
+        //     document.getElementById("inpVerifikasiPegawai").value = ""
+        //     document.getElementById("catatan-tambahan").value = ""
+
+        //     document.getElementById("inpDateDiterima").value = ""
+        //     document.getElementById("inpDateExp").value = ""
+
+        //     document.getElementById("gudangA").checked = false
+        //     document.getElementById("gudangB").checked = false
+        //     document.getElementById("gudangC").checked = false
+
+        //     document.getElementById("inpAvailability").checked =false
+        //     document.getElementById("inpInAvailability").checked =false
+        // }
+    })
+
 })
