da2387d1eaf6db2048cd9d718a571e5c29710abd
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-11-05 20:09:38 +0700

    mindah form cihuy

--- cobareact/helloworld-vite/src/App.jsx
+++ cobareact/helloworld-vite/src/App.jsx
@@ -1,33 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container-fluid p-5">
+        <h1>htmlForM Inventaris Barang PT ABC</h1>
+        <htmlForm action="">
+          <div className="htmlForm-group my-4 px-5">
+            <label htmlFor="txtKodeBarang">Kode Barang</label>
+            <input type="text" id="txtKodeBarang" className="htmlForm-control" name="kodeBarang"
+              placeholder="masukkan kode barang"/>
+          </div>
+
+          <div className="htmlForm-group my-4 px-5">
+            <label htmlFor="txtNamaBarang">Nama Barang</label>
+            <input type="text" name="namaBarang" id="txtNamaBarang" className="htmlForm-control"
+              placeholder="masukkan nama barang"/>
+          </div>
+
+          <div className="htmlForm-group my-4 px-5">
+            <label htmlFor="selectCategory">Kategori Barang</label>
+            <select name="category" id="selectCategory" className="htmlForm-control">
+              <option value="stationery">Stationery</option>
+              <option value="clothing">Clothing</option>
+              <option value="f&b">Food And Beverages</option>
+              <option value="furniture">Furniture</option>
+              <option value="electronics">Electronics</option>
+            </select>
+          </div>
+
+          <div className="htmlForm-group my-4 px-5">
+            <label htmlFor="inpDateDiterima">Tanggal Diterima</label>
+            <input type="date" name="" id="inpDateDiterima"/>
+          </div>
+
+          <div className="htmlForm-group my-4 px-5" id="tanggalExpiredhtmlForm" hidden={true}>
+            <label htmlFor="inpDateExp">Tanggal Expired</label>
+            <input type="date" name="" id="inpDateExp"/>
+          </div>
+
+          <div className="htmlForm-group my-4 px-5">
+            <label htmlFor="inpAvailability">Ketersediaan Barang</label>
+            <div className="htmlForm-check">
+            <label htmlFor="inpAvailability">In Stock</label>
+              <input type="radio" name="availability" id="inpAvailability" value="available"
+                className="htmlForm-check-input"/>
+              <br />
+              <label htmlFor="inpInAvailability">Out Of Stock</label>
+              <input type="radio" name="availability" id="inpInAvailability" value="unavailable"
+                className="htmlForm-check-input"/>
+            </div>
+
+            <div className="htmlForm-group " id="inpNewStock" hidden={true}>
+              <p>Stock Barang (Akumulasi)</p>
+              <input type="number" className="htmlForm-control" name="stockBarang" id="inpStockBarang"/>
+            </div>
+          </div>
+
+          <div className="htmlForm-group my-4 px-5">
+            <label htmlFor="gudang">Gudang Penyimpanan</label>
+            <div className="htmlForm-check">
+              <label htmlFor="gudangA">Gudang A</label>
+              <input type="checkbox" className="htmlForm-check-input" name="gudang" id="gudangA"/>
+              <br />
+              <label htmlFor="gudangB">Gudang B</label>
+              <input type="checkbox" name="gudang" className="htmlForm-check-input" id="gudangB"/>
+              <br />
+              <label htmlFor="gudangC">Gudang C</label>
+              <input type="checkbox" name="gudang" className="htmlForm-check-input" id="gudangC"/>
+            </div>
+          </div>
+
+          <div className="htmlForm-group my-4 px-5">
+            <label htmlFor="catatan-tambahan">Catatan Tambahan</label>
+            <textarea name="" id="catatan-tambahan" className="htmlForm-control" placeholder="catatan tambahan"/>
+          </div>
+
+          <div className="htmlForm-group my-4 px-5">
+            <label htmlFor="inpVerifikasiPegawai">No Verifikasi Pegawai</label>
+            <input type="password" name="" className="htmlForm-control" id="inpVerifikasiPegawai"
+              placeholder="Masukkan Nomor Verifikasi Pegawai"></input>
+              <label htmlFor="chkTogglePass">Tampilkan</label>
+            <input type="checkbox" name="toglePass" className="htmlForm-check-input" id="chkTogglePass"/>
+          </div>
+
+          <br />
+
+          <div className="d-flex gap-3 justify-content-end mx-5">
+            <input className="btn btn-outline-danger my-4 px-5" id="btnClear" type="button" value="clear"/>
+            <input id="btnSubmit" className="btn btn-primary my-4 px-5" type="button" value="submit"/>
+          </div>
+
+
+        </htmlForm>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
