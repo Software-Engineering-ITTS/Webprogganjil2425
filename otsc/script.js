0b52ad0ddc7f9a77e5f6611d256e07e2a684f64b
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-11-05 19:01:19 +0700

    button clear

--- otsc/script.js
+++ otsc/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
+
     // TOGGLE PASS
-    document.getElementById("chkTogglePass").addEventListener('change', function(event){
+    document.getElementById("chkTogglePass").addEventListener('change', function (event) {
         console.log(this.checked)
         var textPass = document.getElementById("inpVerifikasiPegawai")
         if (this.checked) {
@@ -10,42 +10,55 @@ document.addEventListener("DOMContentLoaded", function () {
             textPass.type = 'password'
         }
     })
-    
+
     //TOGGLE CATEGORY
     document.getElementById("selectCategory").addEventListener('change', function (event) {
         var valueSelected = this.selectedOptions[0].value
         var expDate = document.getElementById("tanggalExpiredForm")
-        if(valueSelected === 'f&b'){
+        if (valueSelected === 'f&b') {
             expDate.hidden = false
-        }else{
+        } else {
             expDate.hidden = true
         }
     })
 
     // TOGGLE STOCK
     document.getElementById("inpAvailability").addEventListener('change', function (event) {
-        console.log(this.checked)
-
         var newStock = document.getElementById("inpNewStock")
-        // if(this.checked){
-            newStock.hidden = false
-        // }else{
-        //     newStock.hidden = true
-        // }
+        newStock.hidden = false
+
     })
     document.getElementById("inpInAvailability").addEventListener('click', function (event) {
-        console.log(this.checked)
-
         var newStock = document.getElementById("inpNewStock")
-        // if(this.checked){
-            newStock.hidden = true
-        // }else{
-        //     newStock.hidden = true
-        // }
+        newStock.hidden = true
+
     })
 
-    document.getElementById("btnClear").addEventListener('click', function(event){
+    document.getElementById("btnClear").addEventListener('click', function (event) {
+        const text = "Apakah Anda Yakin Ingin Mengosongkan Form?"
+        if (confirm(text) === true) {
 
+            document.getElementById("txtKodeBarang").value = ""
+            document.getElementById("txtNamaBarang").value = ""
+
+            document.getElementById("selectCategory").selectedIndex = 0
+            document.getElementById("tanggalExpiredForm").hidden = true
+
+            document.getElementById("inpNewStock").hidden = true
+
+            document.getElementById("inpVerifikasiPegawai").value = ""
+            document.getElementById("catatan-tambahan").value = ""
+
+            document.getElementById("inpDateDiterima").value = ""
+            document.getElementById("inpDateExp").value = ""
+
+            document.getElementById("gudangA").checked = false
+            document.getElementById("gudangB").checked = false
+            document.getElementById("gudangC").checked = false
+
+            document.getElementById("inpAvailability").checked =false
+            document.getElementById("inpInAvailability").checked =false
+        }
     })
 
 })
