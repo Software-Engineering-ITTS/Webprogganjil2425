3de03247b4829db2129a9de17ef15d771213e477
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-11-09 20:50:12 +0700

    convert form : slicing

--- cobareact/cots-form/src/App.jsx
+++ cobareact/cots-form/src/App.jsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 // import './App.css'
@@ -14,91 +14,114 @@ function App() {
       <div className="container-fluid p-5">
         <h1>Form Inventaris Barang PT ABC</h1>
         <Form action="">
-          <Form.Group className="htmlForm-group my-4 px-5">
+          <Form.Group className="htmlForm-group my-4 px-5" controlId="inputKodeBarang">
             <Form.Label htmlFor="txtKodeBarang">Kode Barang</Form.Label>
             <Form.Control type="text" id="txtKodeBarang" className="htmlForm-control" name="kodeBarang"
               placeholder="masukkan kode barang" />
           </Form.Group>
 
-          <div className="htmlForm-group my-4 px-5">
-            <label htmlFor="txtNamaBarang">Nama Barang</label>
-            <input type="text" name="namaBarang" id="txtNamaBarang" className="htmlForm-control"
+          <Form.Group className="htmlForm-group my-4 px-5" controlId="inputNamaBarang">
+            <Form.Label htmlFor="txtNamaBarang">Nama Barang</Form.Label>
+            <Form.Control type="text" name="namaBarang" id="txtNamaBarang" className="htmlForm-control"
               placeholder="masukkan nama barang" />
-          </div>
+          </Form.Group>
 
-          <div className="htmlForm-group my-4 px-5">
-            <label htmlFor="selectCategory">Kategori Barang</label>
-            <select name="category" id="selectCategory" className="htmlForm-control">
+          <Form.Group className="htmlForm-group my-4 px-5" controlId="kategoriBarang">
+            <Form.Label htmlFor="selectCategory">Kategori Barang</Form.Label>
+            <Form.Select name="category" id="selectCategory" className="htmlForm-control">
               <option value="stationery">Stationery</option>
               <option value="clothing">Clothing</option>
               <option value="f&b">Food And Beverages</option>
               <option value="furniture">Furniture</option>
               <option value="electronics">Electronics</option>
-            </select>
-          </div>
+            </Form.Select>
+          </Form.Group>
 
-          <div className="htmlForm-group my-4 px-5">
-            <label htmlFor="inpDateDiterima">Tanggal Diterima</label>
-            <input type="date" name="" id="inpDateDiterima" />
-          </div>
+          <Form.Group className="htmlForm-group my-4 px-5" controlId="inputTanggalDiterima">
+            <Form.Label htmlFor="inpDateDiterima">Tanggal Diterima</Form.Label>
+            <Form.Control type="date" name="" id="inpDateDiterima" />
+          </Form.Group>
 
-          <div className="htmlForm-group my-4 px-5" id="tanggalExpiredhtmlForm" hidden={true}>
-            <label htmlFor="inpDateExp">Tanggal Expired</label>
-            <input type="date" name="" id="inpDateExp" />
-          </div>
+          <Form.Group controlId="inputTanggalExpired" className="htmlForm-group my-4 px-5" id="tanggalExpiredhtmlForm" hidden={true}>
+            <Form.Label htmlFor="inpDateExp">Tanggal Expired</Form.Label>
+            <Form.Control type="date" name="" id="inpDateExp" />
+          </Form.Group>
 
-          <div className="htmlForm-group my-4 px-5">
-            <label htmlFor="inpAvailability">Ketersediaan Barang</label>
-            <div className="htmlForm-check">
-              <label htmlFor="inpAvailability">In Stock</label>
-              <input type="radio" name="availability" id="inpAvailability" value="available"
-                className="htmlForm-check-input" />
-              <br />
-              <label htmlFor="inpInAvailability">Out Of Stock</label>
-              <input type="radio" name="availability" id="inpInAvailability" value="unavailable"
-                className="htmlForm-check-input" />
-            </div>
+          <Form.Group className="htmlForm-group my-4 px-5" controlId="inputAvailabilty">
+            <Form.Label htmlFor="inpAvailability">Ketersediaan Barang</Form.Label>
+            <br />
+            <Form.Check
 
-            <div className="htmlForm-group " id="inpNewStock" hidden={true}>
-              <p>Stock Barang (Akumulasi)</p>
-              <input type="number" className="htmlForm-control" name="stockBarang" id="inpStockBarang" />
-            </div>
-          </div>
+              label="In Stock"
+              name="availability"
+              type='radio'
+              id="inpAvailability"
+            />
+            <Form.Check
 
-          <div className="htmlForm-group my-4 px-5">
+              label="Out Of Stock"
+              name="availability"
+              type='radio'
+              id="inpInAvailability"
+            />
+
+            <Form.Group className="htmlForm-group " id="inpNewStock" hidden={true} controlId="stockBarang">
+              <Form.Label>Stock Barang (Akumulasi)</Form.Label>
+              <Form.Control type="number" className="htmlForm-control" name="stockBarang" id="inpStockBarang" />
+            </Form.Group>
+          </Form.Group>
+
+          <Form.Group className="htmlForm-group my-4 px-5" controlId="Gudang Penyimpanan">
             <label htmlFor="gudang">Gudang Penyimpanan</label>
-            <div className="htmlForm-check">
-              <label htmlFor="gudangA">Gudang A</label>
-              <input type="checkbox" className="htmlForm-check-input" name="gudang" id="gudangA" />
-              <br />
-              <label htmlFor="gudangB">Gudang B</label>
-              <input type="checkbox" name="gudang" className="htmlForm-check-input" id="gudangB" />
-              <br />
-              <label htmlFor="gudangC">Gudang C</label>
-              <input type="checkbox" name="gudang" className="htmlForm-check-input" id="gudangC" />
-            </div>
-          </div>
+            <br />
+            <Form.Check
 
-          <div className="htmlForm-group my-4 px-5">
-            <label htmlFor="catatan-tambahan">Catatan Tambahan</label>
-            <textarea name="" id="catatan-tambahan" className="htmlForm-control" placeholder="catatan tambahan" />
-          </div>
+              label="Gudang A "
+              name="gudang"
+              type='checkbox'
+              id="gudangA"
+            />
+            <Form.Check
 
-          <div className="htmlForm-group my-4 px-5">
-            <label htmlFor="inpVerifikasiPegawai">No Verifikasi Pegawai</label>
-            <input type="password" name="" className="htmlForm-control" id="inpVerifikasiPegawai"
-              placeholder="Masukkan Nomor Verifikasi Pegawai"></input>
-            <label htmlFor="chkTogglePass">Tampilkan</label>
-            <input type="checkbox" name="toglePass" className="htmlForm-check-input" id="chkTogglePass" />
-          </div>
+              label="Gudang B "
+              name="gudang"
+              type='checkbox'
+              id="gudangB"
+            />
+            <Form.Check
 
+              label="Gudang C "
+              name="gudang"
+              type='checkbox'
+              id="gudangC"
+            />
+
+          </Form.Group>
+
+          <Form.Group className="htmlForm-group my-4 px-5" controlId="inputCatatan">
+            <Form.Label htmlFor="catatan-tambahan">Catatan Tambahan</Form.Label>
+            <Form.Control as="textarea" name="" id="catatan-tambahan" className="htmlForm-control" placeholder="catatan tambahan" />
+          </Form.Group>
+
+          <Form.Group className="htmlForm-group my-4 px-5" controlId="inputVerifikasiPegawai">
+            <Form.Label htmlFor="inpVerifikasiPegawai">No Verifikasi Pegawai</Form.Label>
+            <Form.Control type="password" name="" className="htmlForm-control" id="inpVerifikasiPegawai"
+              placeholder="Masukkan Nomor Verifikasi Pegawai"></Form.Control>
+            <Form.Check
+              label="Tampilkan "
+              name="toglepass"
+              type='checkbox'
+              id="chkTogglePass"
+            />
+            {/* <label htmlFor="chkTogglePass">Tampilkan</label>
+            <input type="checkbox" name="toglePass" className="htmlForm-check-input" id="chkTogglePass" /> */}
+          </Form.Group>
           <br />
 
-          <div className="d-flex gap-3 justify-content-end mx-5">
-            <input className="btn btn-outline-danger my-4 px-5" id="btnClear" type="button" value="clear" />
-            <input id="btnSubmit" className="btn btn-primary my-4 px-5" type="button" value="submit" />
-          </div>
-
+          <Form.Group className="d-flex gap-3 justify-content-end mx-5">
+            <Button variant="outline-danger" className="btn btn-outline-danger my-4 px-5" id="btnClear" type="button" value="clear" > Clear </Button>
+            <Button variant="primary" id="btnSubmit" className="btn btn-primary my-4 px-5" type="button" value="submit" > Submit </Button>
+          </Form.Group>
 
         </Form>
       </div>
