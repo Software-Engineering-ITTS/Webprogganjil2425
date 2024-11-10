a365a5215db926a6e06e8ab39c0ae686cb17c8a9
GalangTegarM <165545396+GalangTegarM@users.noreply.github.com>
2024-11-10 18:34:58 +0700

    Add files via upload

--- UTS/coba.js
+++ UTS/coba.js
@@ -1,1 +1,9 @@
-const sectionToShow = document.getElementById()
+document.getElementById('ubahwarna').addEventListener('click', function() {
+    if(document.body.style.backgroundColor === "white" ){
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';  
+    }else{
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';    
+    }
+});
