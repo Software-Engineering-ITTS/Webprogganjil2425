16260d78cfd37dfe5faf1257126bc6b594c5e0a2
Mufid Sarnas <mufidsarnas123@gmail.com>
2024-11-10 11:20:02 +0700

    Web Resume & React

---
+++ UTS Web/Web Cv/mufidCv3.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector(".link_menu");
+    const icon = document.querySelector(".ikon_navbar");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
