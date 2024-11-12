ac7319c55b864bd53231f1dcf885c21d1e5ea154
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-11-12 18:02:21 +0700

    simpan data

--- phphelloworld/simpandata.php
+++ phphelloworld/simpandata.php
@@ -3,9 +3,20 @@ include("DbConnection.php");
 
 $nim = $_POST['txt_nim'];
 $nama = $_POST['txt_nama'];
-echo  "NIM: ". $nim;
-echo "<br/>";
-echo "NAMA: " . $nama;
-echo "<br/>";
-var_dump($pdo)
+$status = 'AKTIF';
+// echo  "NIM: ". $nim;
+// echo "<br/>";
+// echo "NAMA: " . $nama;
+// echo "<br/>";
+// var_dump($pdo)
+
+$sql = 'insert into user ( nim, nama, status) values(?, ?, ?)';
+$statement = $pdo->prepare($sql);
+
+if($statement -> execute([$nim, $nama, $status])){
+    echo "data tersimpan!";
+}else{
+    echo "data gagal tersimpan!";
+}
+
 ?>
