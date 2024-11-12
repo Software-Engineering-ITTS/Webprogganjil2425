c0e99bcbaa6b038599f8a13bd42124283a3ec442
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-11-12 17:16:20 +0700

    index.php

---
+++ phphelloworld/index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h1>Data User</h1>
+        <div>
+            <ul>
+                <li><a href="tambahdata.php">Tambah Data</a></li>
+                <li><a href="lihatdata.php">Lihat Data</a></li>
+            </ul>
+        </div>
+    </body>
+
+</html>
