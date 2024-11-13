9f07c064fdd5764da7692c10142b3210cbb13176
Kodon <amirultgr@gmail.com>
2024-11-13 07:31:25 +0700

    project php

---
+++ learnphp/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Data User</h1>
+    <div>
+        <ul>
+            <li>Tambah Data</li>
+            <li>Lihat Data</li>
+            <li>Tambah Data</li>
+        </ul>
+    </div>
+</body>
+</html>
