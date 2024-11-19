5f874556e9b50445a07ce58df5def62a3f850a40
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-11-19 20:11:37 +0700

    asd

--- phphelloworld/backend/lihatdata.php
+++ phphelloworld/backend/lihatdata.php
@@ -2,7 +2,7 @@
 
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Header: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type");
 
 include('db.php');
 
