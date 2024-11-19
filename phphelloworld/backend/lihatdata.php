be8580e92103e90519d5e03ce8fb3464b9d9d7be
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-11-19 17:17:32 +0700

    backend lihat data

---
+++ phphelloworld/backend/lihatdata.php
@@ -0,0 +1,11 @@
+<?php
+include('db.php');
+
+$sql = 'SELECT * FROM user';
+$statement = $pdo -> query($sql);
+$statement -> execute();
+$data = $statement -> fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($data);
+
+?>
