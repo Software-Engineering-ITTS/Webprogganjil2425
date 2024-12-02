5da64790f75cb57f1a9d9c403a05f5f944e86e2d
ArianaHechalinaa <you@example>
2024-12-02 21:59:18 +0700

    Kumpulkan Tugas Laravel

---
+++ test_laravel/tester/database/seeders/FakultasSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\fakultas;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FakultasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        fakultas::create([
+            'nama_fakultas' => 'FIF',
+            'deskripsi' => 'Fakultas Informatika'
+        ]);
+        fakultas::create([
+            'nama_fakultas' => 'FTE',
+            'deskripsi' => 'Fakultas Teknik Elektro'
+        ]);
+        fakultas::create([
+            'nama_fakultas' => 'FEB',
+            'deskripsi' => 'Fakultas Ekonomi Bisnis'
+        ]);
+        //
+    }
+}
