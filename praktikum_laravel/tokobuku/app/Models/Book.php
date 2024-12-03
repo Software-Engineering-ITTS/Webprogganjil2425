a24048b1e99c183ebc876e2e9e6223b67464ef2c
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-12-03 20:08:37 +0700

    edited user model

--- praktikum_laravel/tokobuku/app/Models/Book.php
+++ praktikum_laravel/tokobuku/app/Models/Book.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Book extends Model
 {
-    use HasFactory;
+    use SoftDeletes, HasFactory;
+
+    protected $table = 'books';
+
+    protected $fillable = [
+        'NIM', 'NAMA', "PRODI", "ALAMAT", 'id_fakultas', "fotoktm"
+    ];
+
+    protected $guarded = ['id'];
 }
