1389c3d2c932f1dccbbb69eb0a1c4c89f6ad7b14
Imjustzero <imjustzero@duck.com>
2024-12-03 18:45:25 +0700

    add semantic

--- web/tokobuku/resources/views/index.blade.php
+++ web/tokobuku/resources/views/index.blade.php
@@ -1,13 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Toko Buku</title>
-  @vite('resources/css/app.css')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Toko Buku</title>
+    @vite('resources/css/app.css')
 </head>
+
 <body>
-  <h1 class="bg-slate-500">Hello Dude</h1>
+    {{-- page container --}}
+    <div class="container mx-auto">
+        <nav>
+            {{--  --}}
+        </nav>
+        <header>
+            {{--  --}}
+            <h1 class="bg-slate-500 text-3xl">Toko Buku</h1>
+        </header>
+        <main>
+            {{--  --}}
+        </main>
+        <footer>
+            {{--  --}}
+        </footer>
+    </div>
 </body>
+
 </html>
