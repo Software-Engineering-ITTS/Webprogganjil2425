189e55fe5477b55840ff2055b486d7a1919da382
Imjustzero <imjustzero@duck.com>
2024-12-03 19:37:39 +0700

    add styling

--- web/tokobuku/resources/views/index.blade.php
+++ web/tokobuku/resources/views/index.blade.php
@@ -11,16 +11,55 @@
 
 <body>
     {{-- page container --}}
-    <div class="container mx-auto">
+    <div class="container mx-auto bg-slate-300 p-7 m-9 rounded-lg">
         <nav>
             {{--  --}}
         </nav>
         <header>
             {{--  --}}
-            <h1 class="bg-slate-500 text-3xl">Toko Buku</h1>
+            <h1 class="bg-slate-500 text-7xl text-center rounded-lg p-5 mb-11">Toko Buku</h1>
         </header>
         <main>
-            {{--  --}}
+            {{-- container action  --}}
+            <div class="container bg-blue-300 p-5 rounded-lg mt-5">
+                <h3 class="text-3xl text-center mb-5">Tambah Buku</h3>
+                <label for="judulbuku">Judul Buku</label>
+                <br>
+                <input type="text" name="" id="judulbuku">
+                <br> <br>
+                <label for="judulbuku">Nama Pengarang</label>
+                <br>
+                <input type="text" name="" id="judulbuku">
+                <br> <br>
+                <label for="judulbuku">Tahun Terbit</label> <br>
+                <input type="text" name="" id="judulbuku">
+                <br> <br>
+                <label for="judulbuku">Sinopsis</label> <br>
+                <input type="text" name="" id="judulbuku">
+                <br>
+                <label for="cover-photo">Cover Book</label>
+                <br><br>
+                <input type="file" name="" id="">
+            </div>
+            {{-- container action  --}}
+            <div class="container bg-blue-300 p-5 rounded-lg mt-5">
+                <h3 class="text-3xl text-center mb-5">Show Buku</h3>
+                <label for="judulbuku">Judul Buku</label>
+                <br>
+                <input type="text" name="" id="judulbuku">
+                <br> <br>
+                <label for="judulbuku">Nama Pengarang</label>
+                <br>
+                <input type="text" name="" id="judulbuku">
+                <br> <br>
+                <label for="judulbuku">Tahun Terbit</label> <br>
+                <input type="text" name="" id="judulbuku">
+                <br> <br>
+                <label for="judulbuku">Sinopsis</label> <br>
+                <input type="text" name="" id="judulbuku">
+                <br>
+                <p>Cover Book</p>
+            </div>
         </main>
         <footer>
             {{--  --}}
