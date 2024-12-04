04ba1458fcd9b72afc24da49984b19798216b91b
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-12-04 23:25:15 +0700

    done ga bang

--- praktikum_laravel/tokobuku/resources/views/shared/header.blade.php
+++ praktikum_laravel/tokobuku/resources/views/shared/header.blade.php
@@ -2,9 +2,9 @@
   <div class="container mx-auto px-4 py-3 flex justify-between items-center">
     <a href="{{route('home.index')}}" <h1 class="text-lg font-semibold">Toko Buku</h1></a>
     <nav class="flex space-x-6">
-      <a href="{{route('books.index')}}" class="hover:text-gray-400">Books</a>
-      <a href="{{route('users.index')}}" class="hover:text-gray-400">User</a>
-      <a href="{{route('transactions.index')}}" class="hover:text-gray-400">Transaction</a>
+      <a href="{{route('books.index')}}" class="hover:text-gray-400">Buku</a>
+      <a href="{{route('users.index')}}" class="hover:text-gray-400">Pelanggan</a>
+      <a href="{{route('transactions.index')}}" class="hover:text-gray-400">Transaksi</a>
     </nav>
   </div>
 </header>
