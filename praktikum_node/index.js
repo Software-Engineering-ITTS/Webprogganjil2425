1e3480a32f1085781f6b4f09969ac3a914cb8b33
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-12-10 18:09:31 +0700

    sssssss

--- praktikum_node/index.js
+++ praktikum_node/index.js
@@ -1,1 +1,22 @@
 import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+
+const app = express();
+
+dotenv.config();
+const MONGO_URL = process.env.MONGO_URL
+const PORT = process.env.PORT
+
+mongoose
+.connect(MONGO_URL)
+.then(() => {
+    console.log("Database Connected Succesfully");
+    app.listen(PORT, ()=>{
+        console.log(`Server running on port ${PORT}`);
+    });
+})
+.catch((e) => {
+    console.log(e)
+})
+
