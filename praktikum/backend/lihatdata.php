99a30004767982c6ef17507f07106dda55aaad32
Monica <monica@example.com>
2024-12-17 11:13:08 +0700

    praktikum

---
+++ praktikum/backend/lihatdata.php
@@ -0,0 +1,19 @@
+<?php
+
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+include 'db.php';
+
+$sql = 'select * from user';
+$statement = $pdo->query($sql);
+$statement->execute();
+$data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+//var_dump($data);
+// echo json_encode($data);
+echo json_encode(value: $data);
+?>
+
