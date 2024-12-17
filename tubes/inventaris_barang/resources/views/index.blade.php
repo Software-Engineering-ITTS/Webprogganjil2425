f1ae97706589c07fbb187f5ce37d526168653939
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-12-17 16:56:31 +0700

    todo login

--- tubes/inventaris_barang/resources/views/index.blade.php
+++ tubes/inventaris_barang/resources/views/index.blade.php
@@ -8,17 +8,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
-
 </head>
 
 <body>
     <div id="app">
-        <div class="main-wrapper dark:white dark:bg-gray-900 h-screen">
-            @include('shared.header')
+        <div class="main-wrapper dark:text-white dark:bg-gray-900 h-screen">
             <div class="main-content">
-                @yield('content')
-            </div>
 
+            </div>
         </div>
     </div>
 </body>
