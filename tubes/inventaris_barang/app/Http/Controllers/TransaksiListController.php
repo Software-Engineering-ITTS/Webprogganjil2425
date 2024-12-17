29c6bdf0d99725cb78a99c122f0caff80396f761
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-12-17 17:43:12 +0700

    add transaksi model

---
+++ tubes/inventaris_barang/app/Http/Controllers/TransaksiListController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTransaksiListRequest;
+use App\Http\Requests\UpdateTransaksiListRequest;
+use App\Models\TransaksiList;
+
+class TransaksiListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTransaksiListRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TransaksiList $transaksiList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TransaksiList $transaksiList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTransaksiListRequest $request, TransaksiList $transaksiList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TransaksiList $transaksiList)
+    {
+        //
+    }
+}
