cbd9c10ff6aeaa1082fc07f1ff1f42d7bdcb5517
Imjustzero <imjustzero@duck.com>
2024-12-17 18:23:03 +0700

    add available product

--- web/aircraftstore/resources/views/dashboard.blade.php
+++ web/aircraftstore/resources/views/dashboard.blade.php
@@ -5,12 +5,70 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    {{-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-700 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
                 </div>
+            </div>
+        </div>
+    </div> --}}
+    <div class="container mx-auto bg-gray-900 text-white p-9 rounded-xl">
+        {{-- header --}}
+        <div class="text-white">
+            <h1 class="text-3xl text-center">Available Product</h1>
+        </div>
+        {{-- all product container --}}
+        <div class="grid grid-cols-3">
+            {{-- container each product --}}
+            <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
+                <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
+                <img src="{{ asset('img/v22osprey.jpg') }}" alt="V22 Osprey" class="rounded-md my-3">
+                <p>Type : Tiltrotor military transport aircraft</p>
+                <p>National Origin : United States</p>
+                <p>Manufactured : 1988-Present </p>
+                <p>Price : $99.9999</p>
+            </div>
+            <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
+                <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
+                <img src="{{ asset('img/v22osprey.jpg') }}" alt="V22 Osprey" class="rounded-md my-3">
+                <p>Type : Tiltrotor military transport aircraft</p>
+                <p>National Origin : United States</p>
+                <p>Manufactured : 1988-Present </p>
+                <p>Price : $99.9999</p>
+            </div>
+            <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
+                <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
+                <img src="{{ asset('img/v22osprey.jpg') }}" alt="V22 Osprey" class="rounded-md my-3">
+                <p>Type : Tiltrotor military transport aircraft</p>
+                <p>National Origin : United States</p>
+                <p>Manufactured : 1988-Present </p>
+                <p>Price : $99.9999</p>
+            </div>
+            <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
+                <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
+                <img src="{{ asset('img/v22osprey.jpg') }}" alt="V22 Osprey" class="rounded-md my-3">
+                <p>Type : Tiltrotor military transport aircraft</p>
+                <p>National Origin : United States</p>
+                <p>Manufactured : 1988-Present </p>
+                <p>Price : $99.9999</p>
+            </div>
+            <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
+                <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
+                <img src="{{ asset('img/v22osprey.jpg') }}" alt="V22 Osprey" class="rounded-md my-3">
+                <p>Type : Tiltrotor military transport aircraft</p>
+                <p>National Origin : United States</p>
+                <p>Manufactured : 1988-Present </p>
+                <p>Price : $99.9999</p>
+            </div>
+            <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
+                <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
+                <img src="{{ asset('img/v22osprey.jpg') }}" alt="V22 Osprey" class="rounded-md my-3">
+                <p>Type : Tiltrotor military transport aircraft</p>
+                <p>National Origin : United States</p>
+                <p>Manufactured : 1988-Present </p>
+                <p>Price : $99.9999</p>
             </div>
         </div>
     </div>
