e0609f7e8e996b17df3ac6647b74680ca8ff55be
Imjustzero <imjustzero@duck.com>
2024-12-17 19:24:36 +0700

    fix header dashboard

--- web/aircraftstore/resources/views/dashboard.blade.php
+++ web/aircraftstore/resources/views/dashboard.blade.php
@@ -1,14 +1,12 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 text-center dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <div class="container mx-auto bg-gray-900 text-white p-9 rounded-xl">
+            {{-- header --}}
+            <div class="text-white my-5">
+                <h1 class="text-3xl text-center my-3">Welcome to</h1>
+                <h1 class="text-5xl text-center my-3">Airforce Store</h1>
+            </div>
+        </div>
     </x-slot>
 
-    <div class="container mx-auto bg-gray-900 text-white p-9 rounded-xl my-24">
-        {{-- header --}}
-        <div class="text-white my-5">
-            <h1 class="text-3xl text-center">Welcome to</h1>
-            <h1 class="text-5xl text-center">Airforce Store</h1>
-        </div>
 </x-app-layout>
