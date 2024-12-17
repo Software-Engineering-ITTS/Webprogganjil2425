fe91a5f466ece2fd49114c1d320492b4d85fc1f0
Imjustzero <imjustzero@duck.com>
2024-12-17 19:32:56 +0700

    fix product header

--- web/aircraftstore/resources/views/customer.blade.php
+++ web/aircraftstore/resources/views/customer.blade.php
@@ -1,14 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 text-center dark:text-gray-200 leading-tight">
-            {{ __('Customer') }}
-        </h2>
+        <div class="container mx-auto bg-gray-900 text-white p-9 rounded-xl">
+            {{-- header --}}
+            <div class="text-white my-5">
+                <h1 class="text-3xl text-center">Customer Register</h1>
+            </div>
+            {{-- all product container --}}
+        </div>
     </x-slot>
 
-    <div class="container mx-auto bg-gray-900 text-white p-9 rounded-xl">
-        {{-- header --}}
-        <div class="text-white my-5">
-            <h1 class="text-3xl text-center">Customer Register</h1>
-        </div>
-        {{-- all product container --}}
+
 </x-app-layout>
