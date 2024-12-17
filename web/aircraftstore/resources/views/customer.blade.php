cd01da3bc2839014ed5c56d4ea662d19420cd797
Imjustzero <imjustzero@duck.com>
2024-12-17 19:21:39 +0700

    fix dashboard page

--- web/aircraftstore/resources/views/customer.blade.php
+++ web/aircraftstore/resources/views/customer.blade.php
@@ -8,7 +8,7 @@
     <div class="container mx-auto bg-gray-900 text-white p-9 rounded-xl">
         {{-- header --}}
         <div class="text-white my-5">
-            <h1 class="text-3xl text-center">Available Product</h1>
+            <h1 class="text-3xl text-center">Customer Register</h1>
         </div>
         {{-- all product container --}}
 </x-app-layout>
