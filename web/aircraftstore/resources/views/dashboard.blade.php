2c10c55d87813a0d36e71db3b5e9db31b20e37b6
Imjustzero <imjustzero@duck.com>
2024-12-17 18:41:40 +0700

    add button

--- web/aircraftstore/resources/views/dashboard.blade.php
+++ web/aircraftstore/resources/views/dashboard.blade.php
@@ -16,7 +16,7 @@
     </div> --}}
     <div class="container mx-auto bg-gray-900 text-white p-9 rounded-xl">
         {{-- header --}}
-        <div class="text-white">
+        <div class="text-white my-5">
             <h1 class="text-3xl text-center">Available Product</h1>
         </div>
         {{-- all product container --}}
@@ -29,6 +29,11 @@
                 <p>National Origin : United States</p>
                 <p>Manufactured : 1988-Present </p>
                 <p>Price : $99.9999</p>
+                <div class="flex justify-center">
+                    <div class=" bg-black p-2 w-auto rounded-md hover:bg-gray-900">
+                        <input type="button" value="Buy" class="font-bold">
+                    </div>
+                </div>
             </div>
             <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
                 <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
@@ -37,6 +42,11 @@
                 <p>National Origin : United States</p>
                 <p>Manufactured : 1988-Present </p>
                 <p>Price : $99.9999</p>
+                <div class="flex justify-center">
+                    <div class=" bg-black p-2 w-auto rounded-md hover:bg-gray-900">
+                        <input type="button" value="Buy" class="font-bold">
+                    </div>
+                </div>
             </div>
             <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
                 <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
@@ -45,6 +55,11 @@
                 <p>National Origin : United States</p>
                 <p>Manufactured : 1988-Present </p>
                 <p>Price : $99.9999</p>
+                <div class="flex justify-center">
+                    <div class=" bg-black p-2 w-auto rounded-md hover:bg-gray-900">
+                        <input type="button" value="Buy" class="font-bold">
+                    </div>
+                </div>
             </div>
             <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
                 <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
@@ -53,6 +68,11 @@
                 <p>National Origin : United States</p>
                 <p>Manufactured : 1988-Present </p>
                 <p>Price : $99.9999</p>
+                <div class="flex justify-center">
+                    <div class=" bg-black p-2 w-auto rounded-md hover:bg-gray-900">
+                        <input type="button" value="Buy" class="font-bold">
+                    </div>
+                </div>
             </div>
             <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
                 <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
@@ -61,6 +81,11 @@
                 <p>National Origin : United States</p>
                 <p>Manufactured : 1988-Present </p>
                 <p>Price : $99.9999</p>
+                <div class="flex justify-center">
+                    <div class=" bg-black p-2 w-auto rounded-md hover:bg-gray-900">
+                        <input type="button" value="Buy" class="font-bold">
+                    </div>
+                </div>
             </div>
             <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
                 <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
@@ -69,7 +94,37 @@
                 <p>National Origin : United States</p>
                 <p>Manufactured : 1988-Present </p>
                 <p>Price : $99.9999</p>
+                <div class="flex justify-center">
+                    <div class=" bg-black p-2 w-auto rounded-md hover:bg-gray-900">
+                        <input type="button" value="Buy" class="font-bold">
+                    </div>
+                </div>
+            </div>
+            <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
+                <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
+                <img src="{{ asset('img/v22osprey.jpg') }}" alt="V22 Osprey" class="rounded-md my-3">
+                <p>Type : Tiltrotor military transport aircraft</p>
+                <p>National Origin : United States</p>
+                <p>Manufactured : 1988-Present </p>
+                <p>Price : $99.9999</p>
+                <div class="flex justify-center">
+                    <div class=" bg-black p-2 w-auto rounded-md hover:bg-gray-900">
+                        <input type="button" value="Buy" class="font-bold">
+                    </div>
+                </div>
+            </div>
+            <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
+                <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
+                <img src="{{ asset('img/v22osprey.jpg') }}" alt="V22 Osprey" class="rounded-md my-3">
+                <p>Type : Tiltrotor military transport aircraft</p>
+                <p>National Origin : United States</p>
+                <p>Manufactured : 1988-Present </p>
+                <p>Price : $99.9999</p>
+                <div class="flex justify-center">
+                    <div class=" bg-black p-2 w-auto rounded-md hover:bg-gray-900">
+                        <input type="button" value="Buy" class="font-bold">
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
 </x-app-layout>
