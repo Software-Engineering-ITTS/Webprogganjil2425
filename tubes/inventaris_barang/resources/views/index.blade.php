5b6ac2c254946c3306c5395d72b1756fc54601be
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-12-17 17:33:25 +0700

    todo login page

--- tubes/inventaris_barang/resources/views/index.blade.php
+++ tubes/inventaris_barang/resources/views/index.blade.php
@@ -12,9 +12,31 @@
 
 <body>
     <div id="app">
-        <div class="main-wrapper dark:text-white dark:bg-gray-900 h-screen">
+        <div class="main-wrapper dark:text-white dark:bg-gray-900 h-screen content-center">
             <div class="main-content">
+                <div class="flex justify-evenly">
+                    <div class="flex flex-col">
+                        <p class="text-2xl flex m-10">Selamat Datang Di Inventaris Barang Toko Bu Sudjarmiati</p>
+                        <!-- CARD LOGIN -->
+                        <div class="flex flex-col w-full max-w-sm p-10 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            
+                            <form class="">
+                                <div class="mb-5">
+                                    <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                                    <input type="text" id="username" name="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+                                </div>
+                                <div class="mb-5">
+                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                    <input type="password" id="password" name="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                </div>
 
+                                <button type="submit" class="text-white bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-pink-600">Login</button>
+                            </form>
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
