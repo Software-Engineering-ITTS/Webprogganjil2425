f8b98e30e58d5e16e670c6f196d2b358f013ffca
Imjustzero <imjustzero@duck.com>
2024-12-17 10:25:40 +0700

    add head title, link, n image

--- web/aircraftstore/resources/views/index.blade.php
+++ web/aircraftstore/resources/views/index.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
+    <title>Aircraft Store</title>
+    <link href="{{ asset('img/logoheadtitle.png') }}">
 </head>
 
 <body class="bg-black text-white ">
