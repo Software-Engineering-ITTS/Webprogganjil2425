7f980d1214485a0ffea509718ac74533d1fee44c
Imjustzero <imjustzero@duck.com>
2024-12-18 13:12:42 +0700

    fix template app body styling

--- web/aircraftstore/resources/views/customer.blade.php
+++ web/aircraftstore/resources/views/customer.blade.php
@@ -5,9 +5,14 @@
             <div class="text-white my-5">
                 <h1 class="text-3xl text-center">Customer Register</h1>
             </div>
-            {{-- all product container --}}
         </div>
     </x-slot>
+
+    <div class="container mx-auto bg-gray-900 text-white p-9 rounded-xl">
+        <div class="text-white my-5">
+            {{-- form here --}}
+        </div>
+    </div>
 
 
 </x-app-layout>
--- web/aircraftstore/resources/views/layouts/app.blade.php
+++ web/aircraftstore/resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased">
+<body class="bg-black font-sans antialiased">
     <div class="min-h-screen bg-gray-100 dark:bg-black">
         @include('layouts.navigation')
 
