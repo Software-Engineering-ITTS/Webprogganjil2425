1d5404059171dd22c26f1dba6d8f5f135a6744b5
Imjustzero <imjustzero@duck.com>
2024-12-18 13:00:09 +0700

    fix style reason description

--- web/aircraftstore/resources/views/dashboard.blade.php
+++ web/aircraftstore/resources/views/dashboard.blade.php
@@ -14,36 +14,38 @@
         <p>
 
         <ul class="list-decimal text-justify">
-            <li>Guaranteed Security and Legality
+            <li> <strong>Guaranteed Security and Legality</strong>
                 <p>We ensure that all jets sold meet international standards, with complete documentation, guaranteed
                     legality,
                     and compliance with aviation and international trade regulations.</p>
             </li>
-            <li>Expert and Experienced Team
+            <li> <strong>Expert and Experienced Team</strong>
                 <p> Our team of professionals, experienced in the military aviation industry, is ready to provide
                     consultations
                     and help you select the best aircraft for your requirements.</p>
             </li>
-            <li>Extensive Selection
+            <li><strong>Extensive Selection</strong>
                 <p> Our store offers a wide variety of fighter jets, from legacy models to the latest technology,
                     catering to
                     operational, training, or collector needs.</p>
             </li>
-            <li>Customization and Upgrade Services
+            <li><strong>Customization and Upgrade Services </strong>
                 <p> We offer options for modifications and technological upgrades tailored to your needs, such as adding
                     weapon
                     systems, avionics, or other custom configurations.</p>
             </li>
-            <li>After-Sales Support
+            <li><strong>After-Sales Support </strong>
                 <p>We offer after-sales services, including training, regular maintenance, and technical support,
                     ensuring you
                     can operate the aircraft optimally.</p>
             </li>
-            <li>Reputation and Trust <p> We are trusted by clients from various countries, including governments,
+            <li> <strong>Reputation and Trust</strong>
+                <p> We are trusted by clients from various countries, including governments,
                     security agencies, and private
                     collectors. Our reputation is a testament to the quality of our services</p>
             </li>
-            <li>Global Network <p> With a global network of partners, we have access to manufacturers, spare parts
+            <li><strong>Global Network </strong>
+                <p> With a global network of partners, we have access to manufacturers, spare parts
                     suppliers, and trusted
                     technicians to ensure customer satisfaction.</p>
             </li>
