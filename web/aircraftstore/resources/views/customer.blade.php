feb31fac8c855bd9e97651a084c685dfc2531906
Imjustzero <imjustzero@duck.com>
2024-12-18 13:47:35 +0700

    add form content

--- web/aircraftstore/resources/views/customer.blade.php
+++ web/aircraftstore/resources/views/customer.blade.php
@@ -9,9 +9,36 @@
     </x-slot>
 
     <div class="container mx-auto bg-gray-900 text-white p-9 rounded-xl">
-        <div class="text-white my-5">
-            {{-- form here --}}
-        </div>
+        <form action="">
+            <div class="my-5">
+                <label for="fullname" class="block">Full Name</label>
+                <input type="text" name="fullname" id="fullname" class="rounded-md w-full text-black">
+            </div>
+            <div class="my-5">
+                <label for="phone_number" class="block">Phone Number</label>
+                <input type="text" name="phone_number" id="phone_number" class="rounded-md w-full text-black">
+            </div>
+            <div class="my-5">
+                <label for="address" class="block">Address</label>
+                <input type="text" name="address" id="address" class="rounded-md w-full text-black">
+            </div>
+            <div class="my-5">
+                <label for="  city" class="block">City</label>
+                <input type="text" name="  city" id="  city" class="rounded-md w-full text-black">
+            </div>
+            <div class="my-5">
+                <label for="  province" class="block"> Province</label>
+                <input type="text" name="  province" id="  province" class="rounded-md w-full text-black">
+            </div>
+            <div class="my-5">
+                <label for="  country" class="block"> Country</label>
+                <input type="text" name="  country" id="  country" class="rounded-md w-full text-black">
+            </div>
+            <div class="my-5">
+                <label for="  postal_code" class="block">Postal Code</label>
+                <input type="text" name="  postal_code" id="  postal_code" class="rounded-md w-full text-black">
+            </div>
+        </form>
     </div>
 
 
