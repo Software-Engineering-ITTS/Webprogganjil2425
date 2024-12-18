1237014d5e6359e29d3df3d0ab0eb119f3aa84ba
Imjustzero <imjustzero@duck.com>
2024-12-18 12:39:45 +0700

    add reason why to buy in dshboard

--- web/aircraftstore/resources/views/dashboard.blade.php
+++ web/aircraftstore/resources/views/dashboard.blade.php
@@ -8,5 +8,47 @@
             </div>
         </div>
     </x-slot>
+    <div class="container mx-auto bg-gray-900 text-white p-9 rounded-xl">
+        <h1 class="text-xl text-center">Why buy Aircraft in Our Store? </h1>
+        <p>
+
+        <ul class="list-decimal">
+            <li>Guaranteed Security and Legality
+                <p>We ensure that all jets sold meet international standards, with complete documentation, guaranteed
+                    legality,
+                    and compliance with aviation and international trade regulations.</p>
+            </li>
+            <li>Expert and Experienced Team
+                <p> Our team of professionals, experienced in the military aviation industry, is ready to provide
+                    consultations
+                    and help you select the best aircraft for your requirements.</p>
+            </li>
+            <li>Extensive Selection
+                <p> Our store offers a wide variety of fighter jets, from legacy models to the latest technology,
+                    catering to
+                    operational, training, or collector needs.</p>
+            </li>
+            <li>Customization and Upgrade Services
+                <p> We offer options for modifications and technological upgrades tailored to your needs, such as adding
+                    weapon
+                    systems, avionics, or other custom configurations.</p>
+            </li>
+            <li>After-Sales Support
+                <p>We offer after-sales services, including training, regular maintenance, and technical support,
+                    ensuring you
+                    can operate the aircraft optimally.</p>
+            </li>
+            <li>Reputation and Trust <p> We are trusted by clients from various countries, including governments,
+                    security agencies, and private
+                    collectors. Our reputation is a testament to the quality of our services</p>
+            </li>
+            <li>Global Network <p> With a global network of partners, we have access to manufacturers, spare parts
+                    suppliers, and trusted
+                    technicians to ensure customer satisfaction.</p>
+            </li>
+        </ul>
+        </p>
+
+    </div>
 
 </x-app-layout>
