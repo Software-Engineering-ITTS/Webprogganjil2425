1e3e7d64789f20af2185f598cf28983009cfdbe6
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-12-19 15:18:42 +0700

    fix navbar

--- tubes/inventaris_barang/resources/views/home/home.blade.php
+++ tubes/inventaris_barang/resources/views/home/home.blade.php
@@ -1,14 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="text-center">
-            <h1 class="text-3xl font-bold text-gray-800 mb-4">Welcome, {{ Auth::user()->name }}</h1>
-            <p class="text-gray-600">You are now logged in!</p>
-            <form method="POST" action="{{ route('logout') }}" class="mt-4">
-                @csrf
-                <button type="submit" class="bg-pink-600 text-white px-4 py-2 rounded-lg hover:bg-pink-700">
-                    Logout
-                </button>
-            </form>
-        </div>
+<div class="text-center dark:text-white">
+    <h1 class="text-3xl font-bold mb-4">Welcome, {{ Auth::user()->name }}</h1>
+    <p class="text-gray-600">You are now logged in!</p>
+
+</div>
 @endsection
--- tubes/inventaris_barang/resources/views/shared/header.blade.php
+++ tubes/inventaris_barang/resources/views/shared/header.blade.php
@@ -1,10 +1,16 @@
 <header class="dark:bg-pink-300 text-gray-900 shadow">
-  <div class="container mx-auto px-4 py-3 flex justify-between items-center">
+  <div class="container mx-auto px-4 py-3 flex justify-between">
     <a href="#" <h1 class="text-lg text-gray-900 font-semibold hover:text-white">Inventaris Barang Toko Bu Sudjarmiati</h1></a>
-    <nav class="flex space-x-6">
+    <nav class="flex space-x-6 items-center">
       <a href="#" class="hover:text-white">Stock barang</a>
       <a href="#" class="hover:text-white">Pembelian</a>
       <a href="#" class="hover:text-white">Penjualan</a>
+      <form method="POST" action="{{ route('logout') }}" class="">
+        @csrf
+        <button type="submit" class="bg-pink-600 text-white px-4 py-2 rounded-lg hover:bg-pink-700">
+          Logout
+        </button>
+      </form>
     </nav>
   </div>
 </header>
