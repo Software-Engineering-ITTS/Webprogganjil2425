dde803a83ffface69a831ab514ae7569bc23592b
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-12-19 16:07:21 +0700

    view tabel karyawan

--- tubes/inventaris_barang/app/Http/Controllers/UserController.php
+++ tubes/inventaris_barang/app/Http/Controllers/UserController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index(){
-        return view('karyawan.index');
+        $karyawan = User::where('role', 'karyawan')->paginate(6);
+        return view('karyawan.index', ['karyawan' => $karyawan]);
     }
     
 }
