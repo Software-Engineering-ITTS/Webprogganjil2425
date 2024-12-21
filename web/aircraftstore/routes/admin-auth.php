f7e679039605c7bdb2ec99024e10cc9839a6af05
Imjustzero <imjustzero@duck.com>
2024-12-21 21:52:37 +0700

    Add admin authentication routes for registration and login

---
+++ web/aircraftstore/routes/admin-auth.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+
+Route::middleware('guest:admin')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+
+    Route::post('register', [RegisteredUserController::class, 'store']);
+
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
+
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+});
+
+Route::middleware('auth:admin')->group(function () {
+
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+});
