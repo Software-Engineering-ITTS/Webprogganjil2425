0c7a300a2f7740d900558c1374c5cd5d099195a0
Imjustzero <imjustzero@duck.com>
2024-12-23 17:04:59 +0700

    Rename PurchaseHistory model and update migration to create purchase_histories table

--- web/aircraftstore/database/migrations/2024_12_23_100444_create_purchase_histories_table.php
+++ web/aircraftstore/database/migrations/2024_12_23_100444_create_purchase_histories_table.php
@@ -11,14 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('purchasehistories', function (Blueprint $table) {
+        Schema::create('purchase_histories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('type');
-            $table->string('nationalorigin');
-            $table->string('manufactured');
-            $table->string('price');
-            $table->string('purchasedate');
             $table->timestamps();
         });
     }
@@ -28,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('purchasehistories');
+        Schema::dropIfExists('purchase_histories');
     }
 };
