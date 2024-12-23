6acb84065470a5f483ebd200336adb859fd0fee7
Imjustzero <imjustzero@duck.com>
2024-12-23 13:30:13 +0700

    Add logout functionality to admin dashboard with CSRF protection

--- web/aircraftstore/resources/views/admin/dashboard.blade.php
+++ web/aircraftstore/resources/views/admin/dashboard.blade.php
@@ -5,9 +5,21 @@
 </head>
 
 <body>
+    <nav>
+        <form method="POST" action="{{ route('admin.logout') }}">
+            @csrf
+
+            <x-responsive-nav-link :href="route('admin.logout')"
+                onclick="event.preventDefault();
+                    this.closest('form').submit();">
+                {{ __('Log Out') }}
+            </x-responsive-nav-link>
+        </form>
+    </nav>
     <header>
         {{--  --}}
         <h1>Admin Dashboard</h1>
+
     </header>
     <main>
         {{--  --}}
