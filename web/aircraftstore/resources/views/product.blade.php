47fa5abaedeeba890e29efe1aa0ed5d24ce1ed55
Imjustzero <imjustzero@duck.com>
2024-12-29 01:04:28 +0700

    Update product page title and remove duplicate aircraft entries

--- web/aircraftstore/resources/views/product.blade.php
+++ web/aircraftstore/resources/views/product.blade.php
@@ -3,7 +3,7 @@
         <div class="container mx-auto bg-gray-900 text-white p-9 rounded-xl">
             {{-- header --}}
             <div class="text-white my-5">
-                <h1 class="text-3xl text-center">Available Product</h1>
+                <h1 class="text-3xl text-center">Available Aircraft</h1>
             </div>
         </div>
     </x-slot>
@@ -51,70 +51,5 @@
                     </div>
                 </div>
             </div>
-            <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
-                <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
-                <img src="{{ asset('img/v22osprey.jpg') }}" alt="V22 Osprey" class="rounded-md my-3">
-                <p>Type : Tiltrotor military transport aircraft</p>
-                <p>National Origin : United States</p>
-                <p>Manufactured : 1988-Present </p>
-                <p>Price : $99.9999</p>
-                <div class="flex justify-center">
-                    <div class=" bg-black p-2 w-auto rounded-md hover:bg-gray-900">
-                        <input type="button" value="Buy" class="font-bold">
-                    </div>
-                </div>
-            </div>
-            <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
-                <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
-                <img src="{{ asset('img/v22osprey.jpg') }}" alt="V22 Osprey" class="rounded-md my-3">
-                <p>Type : Tiltrotor military transport aircraft</p>
-                <p>National Origin : United States</p>
-                <p>Manufactured : 1988-Present </p>
-                <p>Price : $99.9999</p>
-                <div class="flex justify-center">
-                    <div class=" bg-black p-2 w-auto rounded-md hover:bg-gray-900">
-                        <input type="button" value="Buy" class="font-bold">
-                    </div>
-                </div>
-            </div>
-            <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
-                <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
-                <img src="{{ asset('img/v22osprey.jpg') }}" alt="V22 Osprey" class="rounded-md my-3">
-                <p>Type : Tiltrotor military transport aircraft</p>
-                <p>National Origin : United States</p>
-                <p>Manufactured : 1988-Present </p>
-                <p>Price : $99.9999</p>
-                <div class="flex justify-center">
-                    <div class=" bg-black p-2 w-auto rounded-md hover:bg-gray-900">
-                        <input type="button" value="Buy" class="font-bold">
-                    </div>
-                </div>
-            </div>
-            <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
-                <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
-                <img src="{{ asset('img/v22osprey.jpg') }}" alt="V22 Osprey" class="rounded-md my-3">
-                <p>Type : Tiltrotor military transport aircraft</p>
-                <p>National Origin : United States</p>
-                <p>Manufactured : 1988-Present </p>
-                <p>Price : $99.9999</p>
-                <div class="flex justify-center">
-                    <div class=" bg-black p-2 w-auto rounded-md hover:bg-gray-900">
-                        <input type="button" value="Buy" class="font-bold">
-                    </div>
-                </div>
-            </div>
-            <div class="bg-gray-700 p-7 rounded-3xl mx-3 my-3">
-                <h1 class="text-xl text-center">Bell Boeing V-22 Osprey</h1>
-                <img src="{{ asset('img/v22osprey.jpg') }}" alt="V22 Osprey" class="rounded-md my-3">
-                <p>Type : Tiltrotor military transport aircraft</p>
-                <p>National Origin : United States</p>
-                <p>Manufactured : 1988-Present </p>
-                <p>Price : $99.9999</p>
-                <div class="flex justify-center">
-                    <div class=" bg-black p-2 w-auto rounded-md hover:bg-gray-900">
-                        <input type="button" value="Buy" class="font-bold">
-                    </div>
-                </div>
-            </div>
-        </div>
+
 </x-app-layout>
