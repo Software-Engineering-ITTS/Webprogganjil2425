7affe30e16d95175f25af631055650145fb13e5e
Imjustzero <imjustzero@duck.com>
2024-12-29 01:12:40 +0700

    Update page titles in admin views to reflect aircraft terminology

--- web/aircraftstore/resources/views/admin/listproduct.blade.php
+++ web/aircraftstore/resources/views/admin/listproduct.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <title>Admin Dashboard</title>
+    <title>List Aircraft</title>
 </head>
 
 <body class="min-h-screen bg-black text-white">
