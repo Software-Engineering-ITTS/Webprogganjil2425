0aacca75588ad68287aa682104a5fd3962dbc4f1
Imjustzero <imjustzero@duck.com>
2024-12-29 02:21:31 +0700

    add store method to AircraftController for aircraft creation

--- web/aircraftstore/app/Http/Controllers/AircraftController.php
+++ web/aircraftstore/app/Http/Controllers/AircraftController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Models\Aircraft;
 
 class AircraftController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validate = $request->validate([
+            'name' => 'required',
+            'type' => 'required',
+            'nationalorigin' => 'required',
+            'manufactured' => 'required',
+            'price' => 'required',
+            'photo' => 'required',
+        ]);
+
+        Aircraft::create($validate);
+        return redirect()->route('/addproduct')->with('success', 'Aircraft Added');
+    }
 }
